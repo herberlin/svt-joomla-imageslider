674ce750445457b884f5452f56deea09acafbf8e
Hans Joachim Herbertz <hans-joachim.herbertz@prodyna.com>
2023-07-15 09:16:30 +0200

    Update for joomla 4

--- mod_svtslider/tmpl/default.php
+++ mod_svtslider/tmpl/default.php
@@ -1,42 +1,32 @@
-<div id="slideshow-mod" class="container">
+<div id="slideshow-mod" >
 		<div id="overlay" class="row-fluid"></div>
 		<div id="slsh" class="row-fluid">
 		<div id="ais_62_wrapper" class="ari-image-slider-wrapper ari-image-slider-loading ari-is-theme-default">
 	        <div id="ais_62" class="ari-image-slider nivoSlider">
-					<img src="/templates/a4joomla-lakeside3r-free/images/sampledata/header_01.png" class="imageslider-item" width="950" height="260">
-					<img src="/templates/a4joomla-lakeside3r-free/images/sampledata/header_02.png" class="imageslider-item" width="950" height="260" style="display:none">
-					<img src="/templates/a4joomla-lakeside3r-free/images/sampledata/header_03.png" class="imageslider-item" width="950" height="260" style="display:none">
-				
+					<img src="/templates/a4joomla-lakeside3r-free/images/sampledata/header_01.png" class="imageslider-item" >
+					<img src="/templates/a4joomla-lakeside3r-free/images/sampledata/header_02.png" class="imageslider-item" >
+					<img src="/templates/a4joomla-lakeside3r-free/images/sampledata/header_03.png" class="imageslider-item">
 	        </div>
 	    </div>  
     </div>
 </div>
 
-<?php 
-/** 
+
 <style>
 #slideshow-mod {
-  padding: 0 0px;
-  position: relative;
-  background: #fff;
+    padding: 0 0;
+    position: relative;
+    max-width: 1350px;
+    background: #3c3e73;
+    margin: auto;
 }
-#slideshow-mod div.moduletable {
-  text-align: justify;
-  margin-bottom: 0px;
+#slideshow-mod .imageslider-item {
+    width: 100%;
+    display: none;
 }
-#slideshow-mod div.moduletable h3 {
-  font-family: Arial, Helvetica, sans-serif;
-  text-align: left;
-  font-size: 18px;
-  text-transform: none;
-  font-weight: normal;
-  letter-spacing: 0px;
-  line-height: 1.2;
-  margin: 0 0 10px 0;
-  padding: 0;
-}
+
 #slsh {
-  border-bottom: 0px dotted #99a;
+  border-bottom: 0 dotted #99a;
 }
 
 #overlay {
@@ -52,14 +42,14 @@
     position: absolute;
 }
 </style>
-*/
-?>
+
 <script>
         console.debug("svtslider");
         const images=[];
         document.querySelectorAll("#slideshow-mod .ari-image-slider img.imageslider-item").forEach(e => images.push(e));
         let counter=0;
-        const size = images.length; 
+        const size = images.length;
+        images[0].style="display:block";
         window.setInterval( () => {
             images[counter%size].style="display:none;";
             images[(counter+1)%size].style="display:block";
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 Replaces Ari Image Slider which does not seem to work with php8
 
+joomla admin
+admin : testtesttest
+
+## mysql 
+dump
+docker exec joomladb /usr/bin/mysqldump -u root --password=example joomla_db > docker/mysql/dump/joomladb.sql
